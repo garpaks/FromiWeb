fc834d2cfda2e5f000113c90b6c8355275386202
unknown <francisco.garcia@metrologia-fromi.com>
2025-09-01 13:55:27 -0600

    Se añadió script y se inició diseño responsivo

---
+++ JS/main.js
@@ -0,0 +1,37 @@
+// Array de los lemas
+const lemas = [
+  {
+    texto:
+      '"Pregúntate si lo que estás haciendo hoy te <br> acerca al lugar en el que quieres estar mañana"',
+    autor: "- Walt Disney -",
+  },
+  {
+    texto: '"Si todos avanzan juntos, entonces <br> el éxito se cuidará solo"',
+    autor: "- Henry Ford -",
+  },
+];
+
+// Variables para el lema
+const lemaTexto = document.querySelector(".lema-container h2");
+const lemaAutor = document.querySelector(".lema-container p ");
+
+// Variable llevar el control del indice actual
+let indiceActual = 0;
+
+// Funcion para cambiar conteido
+function cambiarLema() {
+  const citaActual = lemas[indiceActual];
+
+  lemaTexto.innerHTML = citaActual.texto;
+  lemaAutor.innerHTML = citaActual.autor;
+
+  indiceActual++;
+
+  if (indiceActual >= lemas.length) {
+    indiceActual = 0;
+  }
+}
+
+setInterval(cambiarLema, 5000);
+
+cambiarLema();
