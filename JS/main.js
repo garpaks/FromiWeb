e49b5e51954db4af8a3d6c5599739634dce54ffd
EdwinMunoz115 <edwin.quiroz@metrologia-fromi.com>
2025-09-10 16:14:20 -0600

    se guardan algunos cambios

--- JS/main.js
+++ JS/main.js
@@ -9,6 +9,27 @@ const lemas = [
     texto: '"Si todos avanzan juntos, entonces <br> el éxito se cuidará solo"',
     autor: "- Henry Ford -",
   },
+  {
+    texto: '"El éxito no es la clave de la felicidad. La felicidad es la clave del éxito. Si amas lo que haces, tendrás éxito."',
+    autor: '- Albert Schweitzer -',
+  },
+  {
+    texto: '"La única forma de hacer un gran trabajo es amar lo que haces."',
+    autor: '- Steve Jobs -',
+  },
+  {
+    texto: '"El liderazgo es la capacidad de transformar la visión en realidad."',
+    autor: '- Warren Bennis -',
+  },
+  {
+    texto: '"La innovación es lo que distingue a un líder de los demás."',
+    autor: '- Steve Jobs -',
+  },
+  {
+    texto: '"Nuestra mayor debilidad radica en darnos por vencidos. La forma más segura de tener éxito es siempre intentarlo una vez más."',
+    autor: '- Thomas A. Edison -',
+  },
+
 ];
 
 // Variables para el lema
@@ -32,6 +53,6 @@ function cambiarLema() {
   }
 }
 
-setInterval(cambiarLema, 5000);
+setInterval(cambiarLema, 20000);
 
 cambiarLema();
